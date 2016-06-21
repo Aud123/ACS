30239db20b14cfd79f567d6431568ec4cf45423e
jgabry <jgabry@idfr.net>
2016-06-21 14:35:15 +0200

    Fixing bug with wrong document value

--- Atelier/DATA_SCRAPPING/PhantomJS/get_page_title.js
+++ Atelier/DATA_SCRAPPING/PhantomJS/get_page_title.js
@@ -4,7 +4,7 @@ var page = webPage.create();
 page.open('http://www.ikea.com/fr/fr/catalog/products/S29097737/', function(status) {
 
   var title = page.evaluate(function() {
-    return document.meta;
+    return document.title;
   });
 
   console.log(title);
